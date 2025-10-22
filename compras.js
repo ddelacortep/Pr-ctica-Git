6526abfcf01d4b31f696723b01bc19ae35648799
Danibarrf <daniel.barrufet@gmail.com>
2025-10-22 15:44:16 +0200

    Comentar Usuario

--- compras.js
+++ compras.js
@@ -28,23 +28,46 @@ class Carrito {
   }
 }
 
+/**
+ * Clase que representa a un usuario dentro del sistema.
+ * Cada usuario tiene un nombre, un correo electrónico y un carrito de compras asociado.
+ */
 class Usuario {
+  /**
+   * Constructor de la clase Usuario.
+   * @param {string} nombre - Nombre del usuario.
+   * @param {string} correo - Correo electrónico del usuario.
+   */
   constructor(nombre, correo) {
+    /** @type {string} Nombre del usuario */
     this.nombre = nombre;
+
+    /** @type {string} Correo electrónico del usuario */
     this.correo = correo;
+
+    /** @type {Carrito} Instancia del carrito de compras asociado al usuario */
     this.carrito = new Carrito();
   }
 
+  /**
+   * Agrega un producto al carrito del usuario.
+   * @param {Producto} producto - Objeto que representa el producto a agregar.
+   */
   agregarProductoAlCarrito(producto) {
     this.carrito.agregarProducto(producto);
   }
 
+  /**
+   * Finaliza la compra actual del usuario.
+   * Calcula el total del carrito, muestra un mensaje en consola y vacía el carrito después de la compra.
+   */
   finalizarCompra() {
     const total = this.carrito.calcularTotalCarrito();
     console.log(`Usuario ${this.nombre} ha realizado una compra por un total de ${total}€`);
     this.carrito.vaciarCarrito();
   }
 }
+
 
 
 const producto1 = new Producto("Laptop", 1200, 1);
